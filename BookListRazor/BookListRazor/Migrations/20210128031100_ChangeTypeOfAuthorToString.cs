ef38e32b7427a3a7aa814c6bdc0614ede88426be
DucQuan <quanhien1999@gmail.com>
2021-01-28 11:28:12 +0700

    fisnish Razor project

---
+++ BookListRazor/BookListRazor/Migrations/20210128031100_ChangeTypeOfAuthorToString.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookListRazor.Migrations
+{
+    public partial class ChangeTypeOfAuthorToString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Author",
+                table: "Book",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Author",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
