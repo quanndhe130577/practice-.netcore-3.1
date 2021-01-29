d9e1aeb049f991aa1f786f9faddd0fd1c4c4852c
DucQuan <quanhien1999@gmail.com>
2021-01-29 16:33:03 +0700

    Finish MVC project with .netcore 3.1

---
+++ BookListMVC/Migrations/20210129075943_AddBookToDbMVC_1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookListMVC.Migrations
+{
+    public partial class AddBookToDbMVC_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_books",
+                table: "books");
+
+            migrationBuilder.RenameTable(
+                name: "books",
+                newName: "Books");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Books",
+                table: "Books",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Books",
+                table: "Books");
+
+            migrationBuilder.RenameTable(
+                name: "Books",
+                newName: "books");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_books",
+                table: "books",
+                column: "Id");
+        }
+    }
+}
